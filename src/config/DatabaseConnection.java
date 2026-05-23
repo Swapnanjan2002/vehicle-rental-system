1e65545557118360dc16ce4e39924d99cce1a10f
Swapnanjan Karmakar <swapnanjan.karmakar@gmail.com>
2026-05-24 00:46:11 +0530

    Update database connection credentials

--- src/config/DatabaseConnection.java
+++ src/config/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/enter_database_name";
-    private static final String USER = "root";       // Change this
-    private static final String PASSWORD = "enter_password_here"; // Change this
+    private static final String USER = "root";      
+    private static final String PASSWORD = "enter_password_here"; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
